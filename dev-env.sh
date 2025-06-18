5903361e06d8b792de4bdd73c194af3e19657d08
Shivani Bhardwaj <shivanib134@gmail.com>
2025-06-18 16:55:56 +0530

    add script for first time dev setup

---
+++ dev-env.sh
@@ -0,0 +1,35 @@
+# Install all required packages
+sudo apt-get -y install libpcre2-dev build-essential autoconf \
+automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev \
+pkg-config zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make \
+libmagic-dev libjansson-dev jq git-core sccache
+
+# Install cbindgen
+cargo install --force cbindgen
+
+# Make dir
+mkdir -p $HOME/Orgs/oisf
+cd $HOME/Orgs/oisf
+
+# Clone all projects
+git clone git@github.com:inashivb/suricata.git
+git clone git@github.com:inashivb/suricata-verify.git
+git clone git@github.com:inashivb/suricata-update.git
+git clone git@github.com:inashivb/suricata-intel-index.git
+
+# Go to Suricata and build it
+cd suricata
+make clean
+
+./autogen.sh
+retval=$?
+
+if [ $retval -ne 0 ]; then exit; fi
+
+./configure CC="sccache gcc" CFLAGS="-fno-common -O0 -ggdb3 -Werror -Wchar-subscripts -fno-strict-aliasing -fstack-protector-all -fsanitize=address -fno-omit-frame-pointer -Wshadow" --prefix=/usr/ --sysconfdir=/etc/ --localstatedir=/var/ --disable-shared --enable-debug-validation --enable-debug
+retval=$?
+
+if [ $retval -ne 0 ]; then exit; fi
+
+# Build if configured properly
+make -j8
